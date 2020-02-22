dad34a507d8eb897208c1ceb86f4323192c99a82
phumtutum <48832481+phumtutum@users.noreply.github.com>
2020-02-22 01:16:33 +0000

    Update ConcGraphSearch.scala

--- Concurrent/Practical3/ConcGraphSearch.scala
+++ Concurrent/Practical3/ConcGraphSearch.scala
@@ -1,74 +1,5 @@
 import io.threadcso._
-import scala.collection.mutable.Stack
 
-
-/** A partial queue that terminates if all worker threads are attempting to
-  * dequeue, and the queue is empty.
-  * @param numWorkers the number of worker threads. */
-class TerminatingPartialStack[A](numWorkers: Int){
-  /** Channel for enqueueing. */
-  private val pushChan = ManyOne[A]
-
-  private type ReplyChan = Chan[A]
-
-  /** Channel for dequeueing. */
-  private val popChan = ManyOne[ReplyChan]
-
-  /** Channel for shutting down the queue. */
-  private val shutdownChan = ManyOne[Unit]
-
-  /** Enqueue x.
-    * @throws StopException if the queue has been shutdown. */
-  def push(x: A): Unit = pushChan!x
-
-  /** Attempt to dequeue a value.
-    * @throws StopException if the queue has been shutdown. */
-  def pop: A = {
-    val reply = OneOne[A]
-    popChan!reply
-    reply?()
-  }
-
-  /** Shut down this queue. */
-  def shutdown = attempt{ shutdownChan!(()) }{ }
-  // Note: it's possible that the server has already terminated, in which case
-  // we catch the StopException.
-
-  /** The server process. */
-  private def server = proc("server"){
-    // Currently held values
-    val stack = new Stack[A]()
-    // Queue holding reply channels for current dequeue attempt.
-    val waiters = new Stack[ReplyChan]()
-    // Inv: stack.isEmpty or waiters.isEmpty
-    // Termination: signal to all waiting workers
-    def close = {
-      for(c <- waiters) c.close
-      pushChan.close; popChan.close; shutdownChan.close
-    }
-
-    serve(
-      pushChan =?=> { x => 
-        if(waiters.nonEmpty){ // pass x directly to a waiting dequeue
-          assert(stack.isEmpty); waiters.push!x
-        }
-        else stack.push(x)
-      }
-      |  
-      popChan =?=> { reply =>
-        if(stack.nonEmpty) reply!(stack.pop) // service request immediately
-        else{
-          waiters.push(reply)
-          if(waiters.length == numWorkers) close
-        }
-      }
-      |
-      shutdownChan =?=> { _ => close }
-    )
-  }
-
-  server.fork
-}
 
 
 //STARTING CONCURRENT SEARCH
@@ -92,7 +23,13 @@ class ConcGraphSearch[N](g: Graph[N]) extends GraphSearch[N](g){
     // A single worker
     def worker = proc("worker"){
       repeat{
-        val (n, path) = stack.pop
+        val n = stack.pop
+        for(n1 <- g.succs(n)){
+          if(isTarget(n1)) pathFound!(n1) // done!
+          else stack.push(n1)
+        }
+      }repeat{
+        val n = stack.pop
         for(n1 <- g.succs(n)){
           if(isTarget(n1)) pathFound!(n1) // done!
           else stack.push(n1)
