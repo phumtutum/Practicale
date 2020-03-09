62982a8170363342292574a3549cded46b5865bb
phumtutum <48832481+phumtutum@users.noreply.github.com>
2020-03-09 17:37:01 +0000

    Update BestFirstFrontier.java

--- AIPractical2/BestFirstFrontier.java
+++ AIPractical2/BestFirstFrontier.java
@@ -1,40 +1,49 @@
 package search;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-public class BreadthFirstFrontier implements Frontier{
 
-	Queue<Node> q = new LinkedList<Node>();
-	int cntNodes = 0;
-	int maxNodes = 0;
+public class BestFirstFrontier implements Frontier {
+
+	Comparator<Node> nodeComparator = new Comparator<Node>() {
+		@Override
+		public int compare(Node n1, Node n2) {
+			return n1.f - n2.f;
+		}
+	};
+	
+	PriorityQueue<Node> pq = new PriorityQueue<>(nodeComparator);
+	
+	int maximum = 0;
+	
+	NodeFunction fevaluator;
+	
+	public BestFirstFrontier(NodeFunction fevaluator) {
+		this.fevaluator = fevaluator;
+	}
 	
 	public void addNode(Node element) {
-		q.add(element);
-		cntNodes++;
-		maxNodes = Math.max(maxNodes, cntNodes);
-	}
-
-	public void clearContents() {
-		q.clear();
-		cntNodes = 0;
-	}
-
-	public boolean isEmpty() {
-		return q.isEmpty();
-	}
-
-	public Node removeNode() {
-		assert(!q.isEmpty());
-		cntNodes--;
-		return q.remove();
+		int rez = fevaluator.ev(element);
+		element.setF(rez);
+		pq.add(element);
+		maximum = Math.max(maximum,  pq.size());
 	}
 	
-	public int getCntNodes() {
-		return cntNodes;
+	public void clearContents() {
+		pq.clear();
+	}
+	
+	public boolean isEmpty() {
+		return pq.isEmpty();
+	}
+	
+	public Node removeNode() {
+		return pq.remove();
 	}
 	
 	public int getMaxNodes() {
-		return maxNodes;
+		return maximum;
 	}
-
+	
 }
